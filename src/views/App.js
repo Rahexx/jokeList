226ba2f69eb71f0c70aeb53127b1f55ce12b9afd
Andrzej Łuszcz <endriu1297@gmail.com>
2021-04-16 00:36:05 +0200

    add sort by id and type

--- src/views/App.js
+++ src/views/App.js
@@ -30,9 +30,59 @@ const App = () => {
     return punchline;
   };
 
+  const sortById = (type) => {
+    const copyArray = [...listJokes];
+
+    if (type === 'up') {
+      copyArray.sort(function (a, b) {
+        return a.id - b.id;
+      });
+
+      setListJokes(copyArray);
+    } else {
+      copyArray.sort(function (a, b) {
+        return b.id - a.id;
+      });
+
+      setListJokes(copyArray);
+    }
+  };
+
+  const sortByType = (type) => {
+    const copyArray = [...listJokes];
+
+    if (type === 'up') {
+      copyArray.sort(function (a, b) {
+        if (a.type[0] < b.type[0]) {
+          return -1;
+        }
+        if (a.type[0] > b.type[0]) {
+          return 1;
+        }
+        return 0;
+      });
+
+      setListJokes(copyArray);
+    } else {
+      copyArray.sort(function (a, b) {
+        if (a.type[0] < b.type[0]) {
+          return 1;
+        }
+        if (a.type[0] > b.type[0]) {
+          return -1;
+        }
+        return 0;
+      });
+
+      setListJokes(copyArray);
+    }
+  };
+
   const contextValue = {
     listJokes,
     getPunchline,
+    sortById,
+    sortByType,
   };
 
   return (
