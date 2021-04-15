6cb4c99608024d315de6ddbadd8d42f625b66b75
Andrzej Łuszcz <endriu1297@gmail.com>
2021-04-15 23:11:14 +0200

    add responsive styles for Modal component

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const Modal = () => (
-  <div className='modal w-screen h-screen bg-green-200 absolute top-0 left-0 flex justify-center items-top py-16 z-2'>
-    <i className='fa fa-times absolute top-2 right-5 text-3xl'></i>
-    <h2 className='text-xl px-10 w-full text-center'>punchline</h2>
+  <div className='modal  w-screen h-screen bg-green-200 fixed top-0 left-0 flex justify-center items-top py-16 z-2 lg:w-1/2 lg:h-1/2 lg:top-1/2	lg:left-1/2 lg:transform lg:-translate-x-1/2 lg:-translate-y-1/2 lg:rounded-md lg:h-1/3 xl:w-1/4'>
+    <i className='fa fa-times absolute top-2 right-5 text-3xl lg:top-4 lg:right-7 lg:text-4xl'></i>
+    <h2 className='text-xl px-10 w-full text-center lg:text-2xl'>punchline</h2>
   </div>
 );
 
