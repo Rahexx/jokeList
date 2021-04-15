042cf227bcf92d67ee4e9f287675563364f9dd67
Andrzej Łuszcz <endriu1297@gmail.com>
2021-04-15 19:15:39 +0200

    add tailwind to project

--- src/views/App.js
+++ src/views/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import '../styles/main.css';
 
 function App() {
   return (
     <div>
-      <h1>Hello world</h1>
+      <h1 className='text-2xl font-display'>Hello world</h1>
     </div>
   );
 }
