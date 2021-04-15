a68949a4919ebe8d5c142a4c2a928ae344d9e3b9
Andrzej Łuszcz <endriu1297@gmail.com>
2021-04-15 22:32:14 +0200

    create CellTable component

---
+++ src/components/CellTable/CellTable.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const CellTable = ({ text, otherClass }) => (
+  <td className={`rowTable lg:h-32 lg:text-2xl xl:h-28 ${otherClass}`}>
+    {text}
+  </td>
+);
+
+export default CellTable;
+
+CellTable.defaultProps = {
+  otherClass: '',
+};
+
+CellTable.propTypes = {
+  type: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+  otherClass: PropTypes.string,
+};
--- src/components/tableRow/TableRow.js
+++ src/components/tableRow/TableRow.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import CellTable from '../CellTable/CellTable';
 
 const TableRow = ({ id, type, setup }) => (
   <tr className='flex w-screen lg:h-32 xl:h-28'>
-    <td className='rowTable w-1/6 lg:h-32 lg:text-2xl xl:w-1/12 xl:h-28'>
-      {id}
-    </td>
-    <td className='rowTable w-2/6 lg:h-32 lg:text-2xl xl:w-3/12 xl:h-28'>
-      {type}
-    </td>
-    <td className='rowTable border-r-0 w-3/6 px-3 lg:h-32 lg:text-2xl xl:w-8/12 xl:h-28'>
-      {setup}
-    </td>
+    <CellTable text={id} otherClass='w-1/6 xl:w-1/12' />
+    <CellTable text={type} otherClass='w-2/6 xl:w-3/12' />
+    <CellTable
+      text={setup}
+      otherClass='border-r-0 w-3/6 xl:w-3/12 px-3 xl:w-8/12'
+    />
   </tr>
 );
 
